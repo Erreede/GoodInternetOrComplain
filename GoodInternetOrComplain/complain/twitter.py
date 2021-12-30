fbc08fc6845dc04c4f14c8035f6220bc79768293
Pablo Banuelos <pablo.banuelos@dxc.com>
2021-12-30 17:48:56 +0100

    Improved Twitter message details

--- GoodInternetOrComplain/complain/twitter.py
+++ GoodInternetOrComplain/complain/twitter.py
@@ -5,11 +5,17 @@ from config import config
 def complainOnTwitter(data):
     try:
         #Define the message for the ISP provider
-        downspeed = str(round((round(data['download']) / 1048576), 2)) + " Mb/s"
-        upspeed = str(round((round(data['upload']) / 1048576), 2)) + " Mb/s"
+        hired_bandwidth = 1000
+        hired_bandwidth_str = "1 Gb/s simétricos"
+        downspeed =  round((round(data['download']) / 1048576), 2)
+        downspeed_str = str(downspeed) + " Mb/s"
+        downspeed_quality = str(100 * float(downspeed)/float(hired_bandwidth)) + "%"
+        upspeed = round((round(data['upload']) / 1048576), 2)
+        upspeed_str = str(upspeed) + " Mb/s"
+        upspeed_quality = str(100 * float(upspeed)/float(hired_bandwidth)) + "%"
         ping = str(round(data['ping'], 2)) + " ms"
         
-        message = "La velocidad de mi conexión es:\r\n-Bajada: " + downspeed + "\r\n-Subida: " + upspeed + "\r\n-Ping: " + ping + "\r\nDetalle: " + data['png_url']
+        message = "La velocidad de mi conexión es:\r\n-Bajada: " + downspeed_str + "\r\n-Subida: " + upspeed_str + "\r\n-Ping: " + ping + "\r\n\r\nDetalle: " + data['png_url'] + "\r\n\r\nContratado: " + hired_bandwidth_str + ", lo que supone un " + downspeed_quality + " de la bajada y un " + upspeed_quality + " de la subida."
 
         #Create the Twitter wrapper and attach the screenshot of the speedtest
         t = Twitter(auth=OAuth(config.twitterAPI_oAuthToken, config.twitterAPI_oAuthSecret, config.twitterAPI_appClientID, config.twitterAPI_appClientSecret))
