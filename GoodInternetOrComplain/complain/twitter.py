9ee5bc21dea078fb22317aabef430a767cc35921
Pablo Banuelos <pablo.banuelos@dxc.com>
2021-12-30 17:36:58 +0100

    Improved Twitter message with details

--- GoodInternetOrComplain/complain/twitter.py
+++ GoodInternetOrComplain/complain/twitter.py
@@ -4,8 +4,15 @@ from config import config
 
 def complainOnTwitter(data):
     try:
+        #Define the message for the ISP provider
+        downspeed = str(round((round(data['download']) / 1048576), 2)) + " Mb/s"
+        upspeed = str(round((round(data['upload']) / 1048576), 2)) + " Mb/s"
+        ping = str(round(data['ping'], 2)) + " ms"
+        
+        message = "La velocidad de mi conexión es:\r\n-Bajada: " + downspeed + "\r\n-Subida: " + upspeed + "\r\n-Ping: " + ping + "\r\nDetalle: " + data['png_url']
+
+        #Create the Twitter wrapper and attach the screenshot of the speedtest
         t = Twitter(auth=OAuth(config.twitterAPI_oAuthToken, config.twitterAPI_oAuthSecret, config.twitterAPI_appClientID, config.twitterAPI_appClientSecret))
-        message = data['png_url']
         tweet = t.statuses.update(status=message)
         if 'id' in  tweet:
             data['tweet_id'] = tweet['id']
