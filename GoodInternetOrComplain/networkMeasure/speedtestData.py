99f3458826a4d05f091c4e43c380ec18b8c2cc9c
Pablo Banuelos <pablo.banuelos@dxc.com>
2022-01-03 15:10:17 +0100

    Added secure=True to improve server detection

--- GoodInternetOrComplain/networkMeasure/speedtestData.py
+++ GoodInternetOrComplain/networkMeasure/speedtestData.py
@@ -2,12 +2,12 @@ import speedtest
 
 def performTest():
     try:
-        s = speedtest.Speedtest()
-        s.get_closest_servers()
+        s = speedtest.Speedtest(secure=True)
         s.download()
         s.upload(pre_allocate=False)
         results = s.results.dict()
         results['png_url'] = s.results.share()
         return results
-    except:
-        return {'status': "Error"}
+    except Exception as err:        
+        print(f'Error occurred: {err}')
+        return {}
