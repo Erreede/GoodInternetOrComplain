a4355bd7bf8c9877a588a24564b7951e399bb263
Pablo Banuelos <pablo.banuelos@dxc.com>
2022-01-03 15:19:46 +0100

    Added env var twitterComplain to toggle on or off the tweet

--- GoodInternetOrComplain/GoodInternetOrComplain.py
+++ GoodInternetOrComplain/GoodInternetOrComplain.py
@@ -1,12 +1,13 @@
 from dbCon import dbCon
 from networkMeasure import speedtestData
 from complain import twitter
+from config.config import twitterComplain
 
 def run():
     resultsdbCon = dbCon.createConnection()
     #Perform speedtest using the Ookla SpeedTest lib
     testResults = speedtestData.performTest()
-    if 'png_url' in testResults:
+    if 'png_url' in testResults and twitterComplain:
         #Send the speedtest data to the twitter wrapper to post the message
         twitter.complainOnTwitter(testResults)
         #Save the data in the database
