5778295f7d325160deef1dda6722f16899323d27
Pablo Banuelos <pablo.banuelos@dxc.com>
2022-01-04 09:57:42 +0100

    excluded empy test results

--- GoodInternetOrComplain/GoodInternetOrComplain.py
+++ GoodInternetOrComplain/GoodInternetOrComplain.py
@@ -7,11 +7,12 @@ def run():
     resultsdbCon = dbCon.createConnection()
     #Perform speedtest using the Ookla SpeedTest lib
     testResults = speedtestData.performTest()
-    if 'png_url' in testResults and twitterComplain:
-        #Send the speedtest data to the twitter wrapper to post the message
-        twitter.complainOnTwitter(testResults)
+        
     #Save the data in the database
-    dbCon.insertResults(resultsdbCon, testResults)
+    if len(testResults) > 0: 
+        if twitterComplain:
+            twitter.complainOnTwitter(testResults)
+        dbCon.insertResults(resultsdbCon, testResults)
     resultsdbCon.close()
 
 if __name__ == '__main__':
