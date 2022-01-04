8c2551688d0b68af7f27aaac4552a97cea1d3c91
Pablo Banuelos <pablo.banuelos@dxc.com>
2022-01-04 09:51:20 +0100

    Modified environment variables

--- GoodInternetOrComplain/config/config.py
+++ GoodInternetOrComplain/config/config.py
@@ -6,4 +6,4 @@ twitterAPI_oAuthToken = os.getenv('twitterAPI_oAuthToken')
 twitterAPI_oAuthSecret = os.getenv('twitterAPI_oAuthSecret')
 twitterAPI_appClientID = os.getenv('twitterAPI_appClientID')
 twitterAPI_appClientSecret = os.getenv('twitterAPI_appClientSecret')
-twitterComplain = os.getenv('twitterComplain')
+twitterComplain = os.getenv('twitterComplain', False) == 'True'
