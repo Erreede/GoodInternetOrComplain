4e77b7d59c42e8aad78f1c1fa19c595c2f741708
Pablo Banuelos <pablo.banuelos@dxc.com>
2022-03-26 13:13:47 +0100

    Fix to link creation

--- GoodInternetOrComplain/networkMeasure/speedtestData.py
+++ GoodInternetOrComplain/networkMeasure/speedtestData.py
@@ -6,7 +6,10 @@ def performTest():
         s.download()
         s.upload(pre_allocate=False)
         results = s.results.dict()
-        results['png_url'] = s.results.share()
+        try:
+            results['png_url'] = s.results.share()
+        except:
+            pass
         return results
     except Exception as err:        
         print(f'Error occurred: {err}')
